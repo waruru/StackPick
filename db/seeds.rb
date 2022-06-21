e05ff5e1f3a830914c0927108c02daf2356cda63
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-06-21 23:04:38 +0900

    seed更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,8 @@ unless User.find_by(email: "test@example.com").posts.exists?
   Post.create(title: "twitter", url: "https://twitter.com/", user: user)
   Post.create(title: "google", url: "https://google.com/", user: user)
   Post.create(title: "CirKit", url: "https://www.cirkit.jp/", user: user)
+  50.times do |t|
+    Post.create(title: "example##{t}", url: "https://example.com/#{t}", user: user)
+  end
   puts "Post is created"
 end
