fa7af38ed90d14d461c9741905c002660f0fab75
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-06-24 00:30:56 +0900

    op画像のurlを格納するコールバックメソッド追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,17 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
+
+  before_save :set_ogp_url
+
+  private
+  def set_ogp_url
+    begin
+      page = Mechanize.new.get(self.url)
+      url = page.at('meta[property="og:image"]')[:content]
+    rescue => e
+      url = ""
+    end
+    self.ogp_url = url
+  end
 end
