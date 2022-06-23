4a94512f14c44c119c5adecf94bae8b70b655402
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-06-24 00:31:36 +0900

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,13 @@ end
 
 unless User.find_by(email: "test@example.com").posts.exists?
   user = User.find_by(email: "test@example.com")
-  Post.create(title: "twitter", url: "https://twitter.com/", user: user)
+  Post.create(title: "twitter", url: "https://twitter.com/2002_wrr", user: user)
   Post.create(title: "google", url: "https://google.com/", user: user)
   Post.create(title: "CirKit", url: "https://www.cirkit.jp/", user: user)
-  50.times do |t|
+  Post.create(title: "白雪姫", url: "https://www.netflix.com/watch/80220567?trackId=155573560", user: user)
+  Post.create(title: "Qiita", url: "https://qiita.com/koji-koji/items/4886dda6d287c7f2dc1b", user: user)
+  Post.create(title: "Railsガイド", url: "https://railsguides.jp/", user: user)
+  20.times do |t|
     Post.create(title: "example##{t}", url: "https://example.com/#{t}", user: user)
   end
   puts "Post is created"
