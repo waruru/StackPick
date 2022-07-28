bb892e2e4a2150d69b2a747648498d37650558d6
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-07-28 13:33:04 +0900

    production.rb fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,6 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   config.assets.initialize_on_precompile=false
-  config.assets.js_compressor = :uglifier
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
