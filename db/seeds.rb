28137247f01d0e9e901f68b9133b0db0198bf5c4
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-07-31 14:31:00 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,21 +11,21 @@ unless User.find_by(email: "test@example.com").posts.exists?
   Post.create(title: "白雪姫", url: "https://www.netflix.com/watch/80220567?trackId=155573560", user: user)
   Post.create(title: "Qiita", url: "https://qiita.com/koji-koji/items/4886dda6d287c7f2dc1b", user: user)
   Post.create(title: "Railsガイド", url: "https://railsguides.jp/", user: user)
-  20.times do |t|
-    Post.create(title: "example##{t}", url: "https://example.com/#{t}", user: user)
-  end
+  # 20.times do |t|
+  #   Post.create(title: "example##{t}", url: "https://example.com/#{t}", user: user)
+  # end
   puts "Post is created"
 end
 
-unless User.find_by(email: "test@example.com").tags.exists?
-  user = User.find_by(email: "test@example.com")
-  tags = [
-    {name: "アニメ", user: user},
-    {name: "ドラマ", user: user},
-    {name: "映画", user: user},
-    {name: "技術記事", user: user},
-    {name: "Qiita", user: user}
-  ]
-  Tag.create(tags)
-  puts "Tag is created"
-end
+# unless User.find_by(email: "test@example.com").tags.exists?
+#   user = User.find_by(email: "test@example.com")
+#   tags = [
+#     {name: "アニメ", user: user},
+#     {name: "ドラマ", user: user},
+#     {name: "映画", user: user},
+#     {name: "技術記事", user: user},
+#     {name: "Qiita", user: user}
+#   ]
+#   Tag.create(tags)
+#   puts "Tag is created"
+# end
