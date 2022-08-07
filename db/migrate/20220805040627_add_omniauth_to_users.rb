f99324e506fca378f2a9d239033d47aa75f38201
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-08-08 03:46:22 +0900

    Fix add column migration file

--- db/migrate/20220805040627_add_omniauth_to_users.rb
+++ db/migrate/20220805040627_add_omniauth_to_users.rb
@@ -1,8 +1,8 @@
 class AddOmniauthToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :provider, :string
-    add_column :users, :uid,      :string
-    add_column :users, :name,     :string
-    add_column :users, :token,    :string
+    add_column :users, :name,     :string, after: :email
+    add_column :users, :token,    :string, after: :encrypted_password
+    add_column :users, :uid,      :string, after: :encrypted_password
+    add_column :users, :provider, :string, after: :encrypted_password
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -51,16 +51,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_06_144550) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "name"
     t.string "encrypted_password", default: "", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "token"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "provider"
-    t.string "uid"
-    t.string "name"
-    t.string "token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
