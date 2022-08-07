58761fc4dec8f0bf4b95cfc558fe4d35cc0b264c
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-08-08 04:07:09 +0900

    Update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,16 @@ end
 
 unless User.find_by(email: "test@example.com").posts.exists?
   user = User.find_by(email: "test@example.com")
-  Post.create(title: "twitter", url: "https://twitter.com/2002_wrr", user: user)
-  Post.create(title: "google", url: "https://google.com/", user: user)
-  Post.create(title: "CirKit", url: "https://www.cirkit.jp/", user: user)
-  Post.create(title: "白雪姫", url: "https://www.netflix.com/watch/80220567?trackId=155573560", user: user)
-  Post.create(title: "Qiita", url: "https://qiita.com/koji-koji/items/4886dda6d287c7f2dc1b", user: user)
-  Post.create(title: "Railsガイド", url: "https://railsguides.jp/", user: user)
+  posts = [
+    {title: "Qiita", url: "https://qiita.com/koji-koji/items/4886dda6d287c7f2dc1b", user: user, public: true},
+    {title: "Railsガイド", url: "https://railsguides.jp/", user: user, public: true},
+    {title: "白雪姫", url: "https://www.netflix.com/watch/80220567?trackId=155573560", user: user, public: true},
+    {title: "CirKit", url: "https://www.cirkit.jp/", user: user, public: true},
+    {title: "google", url: "https://google.com/", user: user, public: false},
+    {title: "twitter", url: "https://twitter.com/2002_wrr", user: user, public: false}
+  ]
+  Post.create(posts)
+
   # 20.times do |t|
   #   Post.create(title: "example##{t}", url: "https://example.com/#{t}", user: user)
   # end
