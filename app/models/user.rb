e1db7ea2233f09e79686bb635d89f07dc5b15112
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-08-12 14:02:41 +0900

    IconUploader追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  mount_uploader :icon, IconUploader
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2, :twitter, :github]
