4bd6adbed3b15a2bc03930d6f5f43cdc1583107c
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-09-05 23:01:43 +0900

    エラーを出力

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,7 +39,10 @@ class PostsController < ApplicationController
         format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html {
+          flash[:alert] = @post.errors.full_messages
+          render :new, status: :unprocessable_entity
+        }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
