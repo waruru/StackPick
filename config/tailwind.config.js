d22c29de5047b0f41ae5bf7c1f38b79585d86ecb
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-11-01 00:42:54 +0900

    daisyUIの設定追記

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,5 +6,18 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
+
   plugins: [require("daisyui")],
+
+  // daisyUIの設定(https://daisyui.com/docs/config/)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
 }
