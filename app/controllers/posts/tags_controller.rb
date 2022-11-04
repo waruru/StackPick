e0a419a20a69b98400291d1e25b522e6c3d93524
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-11-05 03:26:34 +0900

    タグ削除処理を追記

--- app/controllers/posts/tags_controller.rb
+++ app/controllers/posts/tags_controller.rb
@@ -1,4 +1,9 @@
 class Posts::TagsController < ApplicationController
   def index
   end
+
+  def remove
+    tag = Tag.find(params[:id])
+    render turbo_stream: turbo_stream.remove("tag_#{tag.id}")
+  end
 end
