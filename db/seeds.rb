efa8f7737b7a168ce81f4a28150b4833c7711d05
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-11-15 14:55:57 +0900

    seedファイル更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,32 +3,26 @@ unless User.exists?(email: "test@example.com")
   puts "User is created"
 end
 
-unless User.find_by(email: "test@example.com").posts.exists?
-  user = User.find_by(email: "test@example.com")
-  posts = [
-    {title: "Qiita", url: "https://qiita.com/koji-koji/items/4886dda6d287c7f2dc1b", user: user, published: true},
-    {title: "Railsガイド", url: "https://railsguides.jp/", user: user, published: true},
-    {title: "白雪姫", url: "https://www.netflix.com/watch/80220567?trackId=155573560", user: user, published: true},
-    {title: "CirKit", url: "https://www.cirkit.jp/", user: user, published: true},
-    {title: "google", url: "https://google.com/", user: user, published: false},
-    {title: "twitter", url: "https://twitter.com/2002_wrr", user: user, published: false}
-  ]
-  Post.create(posts)
-
-  # 20.times do |t|
-  #   Post.create(title: "example##{t}", url: "https://example.com/#{t}", user: user)
-  # end
-  puts "Post is created"
-end
-
 unless Tag.exists?
   tags = [
-    {name: "アニメ"},
-    {name: "ドラマ"},
-    {name: "映画"},
-    {name: "技術記事"},
-    {name: "Qiita"}
+    {id: 1, name: "アニメ"},
+    {id: 2, name: "ドラマ"},
+    {id: 3, name: "映画"},
+    {id: 4, name: "技術記事"},
+    {id: 5, name: "Qiita"}
   ]
   Tag.create(tags)
   puts "Tag is created"
+end
+
+unless User.find_by(email: "test@example.com").posts.exists?
+  user = User.find_by(email: "test@example.com")
+  posts = [
+    {title: "Railsでのスクレイピング", url: "https://qiita.com/koji-koji/items/4886dda6d287c7f2dc1b", user: user, published: true},
+    {title: "Railsガイド", url: "https://railsguides.jp/", user: user, published: true},
+    {title: "白雪姫", url: "https://www.netflix.com/watch/80220567?trackId=155573560", user: user, published: true},
+    {title: "CirKit ホームページ", url: "https://www.cirkit.jp/", user: user, published: true},
+  ]
+  Post.create(posts)
+  puts "Post is created"
 end
