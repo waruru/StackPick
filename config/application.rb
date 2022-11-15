7f77345345a1aa733c4dfe41b6f0b42126085964
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-11-15 10:10:26 +0900

    アプリ名を修正

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module StackAppRails
+module StackPick
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
